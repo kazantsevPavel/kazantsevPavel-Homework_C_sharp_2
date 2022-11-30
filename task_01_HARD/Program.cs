f0d9dacba5421df62aee95048aa64d3859e443e1
Pavel <kazantzevp@yandex.ru>
2022-11-30 15:26:44 +0500

    Homework is finished

---
+++ task_01_HARD/Program.cs
@@ -0,0 +1,33 @@
+﻿// Прогамма принимает на вход N чисел (число пользователя) и ищет среди них квадраты друг друга
+Console.WriteLine("Сколько чисел будет в вашем массиве?");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int i = 0;
+int j = 0;
+
+while (i < N)
+{
+    Console.WriteLine("Введите число № " + (i + 1));
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+i=0;
+
+while (j < N)
+{
+    while (i < N)
+    {
+        
+        if (array[j] == array[i] * array[i])
+        {
+            Console.WriteLine($"{array[j]} это квадрат {array[i]}");
+            i++;
+        }
+        else i++;
+    }
+    i = 0;
+    j++;
+}
+
+Console.WriteLine ();
---
+++ task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Здравствуйте, сэр! Подскажите, какой сегодня день недели?");
+Console.WriteLine("Просто напишите цифру, сэр!");
+Console.WriteLine("1.Monday");
+Console.WriteLine("2.Tuesday");
+Console.WriteLine("3.Wednesday");
+Console.WriteLine("4.Thursday");
+Console.WriteLine("5.Friday");
+Console.WriteLine("6.Saturday");
+Console.WriteLine("7.Sunday");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 1 || a > 7) Console.WriteLine("Сэр, такого дня недели не существует");
+
+else
+{
+    if (a > 5) Console.WriteLine("Сэр, сегодня выходной! Вот ваше шампанское, сэр!");
+    else Console.WriteLine("Сэр, сегодня рабочий день! Вот ваш чай, сэр!");
+}
+Console.WriteLine("Хорошего дня, сэр!");
